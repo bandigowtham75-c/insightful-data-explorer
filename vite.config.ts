cf2d228fc0ac6eb40fff21943d44c918f883f36e
bandigowtham75-c <bandigowtham75@gmail.com>
2026-03-17 19:15:50 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/insightful-data-explorer/',   // 👈 ADD THIS
+})
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
